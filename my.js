1a9c28f7f070d69807ad309007ae0cadadfe1758
Natalya <natalya-medvedeva95@mail.ru>
2022-02-13 11:21:53 +0300

    :test

---
+++ my.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// задание 1
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+// задание 2
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+// задание 3
+
+const firstQuestion = prompt('Один из последних просмотренных фильмов?');
+
+const secondQuestion = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[firstQuestion]=secondQuestion;
