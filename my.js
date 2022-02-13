a7873e0837f4410ae7239c55115acf97687ca001
Natalya <natalya-medvedeva95@mail.ru>
2022-02-13 13:18:10 +0300

    :optimization of the application by functions

--- my.js
+++ my.js
@@ -1,9 +1,21 @@
 "use strict";
 
-// урок 1 задание 1
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-// задание 2
+
+let numberOfFilms;
+
+// isNaN(numberOfFilms) - проверяет введённый ответ на число, если не число, то вернётся правда
+// функция старт не позволит пользователю вводить вместо ответа пустую строку или не число или просто нажать кнопку ок или отмена
+
+function start (){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+    }
+}
+
+
+start ();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,7 +24,7 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
-// задание 3
+
 
 const firstQuestion = prompt('Один из последних просмотренных фильмов?');
 
@@ -23,31 +35,61 @@ personalMovieDB.movies[firstQuestion]=secondQuestion;
 console.log(personalMovieDB);
 
 
-// урок 2
-
-  for (let i = 0; i < 1; i++){
-      const firstQuestion = prompt('Один из последних просмотренных фильмов?'),
-            secondQuestion = prompt('На сколько оцените его?');
-      if (firstQuestion != '' && firstQuestion != null && firstQuestion.length < 50 && secondQuestion != '' && secondQuestion != null &&secondQuestion.length < 50){
-             personalMovieDB.movies[firstQuestion]=secondQuestion;
-             console.log('done');
-      }else {
-              i--;
-              console.log('error');
-      }
-  }
 
 
-if (personalMovieDB.count < 10){
-    console.log('Просмотрено довольно мало фильмов');
-}else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
-    console.log('Вы классический зритель');
-}else if (personalMovieDB.count >= 30){
-    console.log('Вы киноман');
-}else{
-    console.log('Произошла ошибка');
+function rememberMyFilms () {
+    for (let i = 0; i < 1; i++){
+        const firstQuestion = prompt('Один из последних просмотренных фильмов?'),
+              secondQuestion = prompt('На сколько оцените его?');
+        if (firstQuestion != '' && firstQuestion != null && firstQuestion.length < 50 && secondQuestion != '' && secondQuestion != null &&secondQuestion.length < 50){
+               personalMovieDB.movies[firstQuestion]=secondQuestion;
+               console.log('done');
+        }else {
+                i--;
+                console.log('error');
+        }
+    }
 }
 
+rememberMyFilms ();
+
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10){
+        console.log('Просмотрено довольно мало фильмов');
+    }else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        console.log('Вы классический зритель');
+    }else if (personalMovieDB.count >= 30){
+        console.log('Вы киноман');
+    }else{
+        console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+
+
+function showMyDB (hidden) {
+    if(!hidden){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+// в данной функции пользователю трижды задаётся вопрос, цикл начинается с единицы, так как пользователь не знает об индексах, вопрос задаётся в бэктиках, что в него сразу вставился ответ от пользователя, далее единиица отнимается, чтобы цикл начинался с 0 и совпадал с ответом, есил данный минус 1 убрать, то в консоли в ответах появится 4 ячейка, которой быть не должно
+
+function writeYourGenre () {
+    for (let i = 1; i <= 3; i++){
+        let question = prompt(`Ваш любимый жанр под номером ${i}`);
+           personalMovieDB.genres [i-1] = question;
+    }
+}
+writeYourGenre();
 
 // цикл while (4задание  2 урока)
 // let i = 0;
@@ -77,3 +119,6 @@ if (personalMovieDB.count < 10){
 //       }
 // }
 // while(i < 1);
+
+
+
